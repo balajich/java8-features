8cdf2aa2b98f8151f84a5a93bc2c8fa45de4a86d
balaji <balaji.chopparapu@gmail.com>
2022-07-11 10:45:06 -0700

    Java11 Dev. Exam preperation

--- src/main/java/CreatingListUsingVarArgs.java
+++ src/main/java/CreatingListUsingVarArgs.java
@@ -1,2 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class CreatingListUsingVarArgs {
+    public static void main(String[] args) {
+        List<String> list1= Arrays.asList("A","B");
+        List<String> list2=List.of("A","B");
+        List<String> list3= Arrays.asList(new String[]{"A","B"});
+        //All of the above create immutable list
+        //list1.add("C");
+       // list2.add("C");
+        //list3.add("C");
+
+    }
 }
